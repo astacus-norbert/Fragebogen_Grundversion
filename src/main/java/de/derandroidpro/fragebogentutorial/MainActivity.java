c2757230e9040d51f17eeead236c57ae1b7642dd
astacus-norbert <norbert.wiacek@astacus-gmbh.de>
2016-06-02 22:02:44 +0200

    next stage images will need to be displayed

--- src/main/java/de/derandroidpro/fragebogentutorial/MainActivity.java
+++ src/main/java/de/derandroidpro/fragebogentutorial/MainActivity.java
@@ -40,14 +40,59 @@ public class MainActivity extends AppCompatActivity {
     int ncount = 0; // NEIN-Zähler
     int qvector = 0; //Fragelisten-Vektor
     public String antwort = "1. Test!";
-    public List<String> questions = Arrays.asList("Frage 1: Na, geht's Dir gut?", "Frage 2: Magst Du Tee?", "Frage 3: Tee mit Zucker?", "Frage 4: Magst Du Tee mit Milch", "Frage 5: Magst Du Milch dazu?", "Frage 6: Oder willst Du lieber Zitrone dazu?", "Frage 7: Etwa Milch mit Zitrone im Tee? ", "Frage 8: Lieber von vorn. Magst Du Kaffee?", "Frage 9: Mit Zucker?", "Frage 10: Mit Milch? ", "Frage 11: Cappuccino vieleicht?", "Frage 12: Mit Schokostreuseln?", "Frage 13: ???", "Frage : ??", "Frage : ?", "Frage : ", "Frage : ");
-//    JProgressBar proba; //Statusanzeige
+    public List<String> questions = Arrays.asList(//
+            "Frage 1: Magst Du Kräutertee?",
+            "Frage 2: Magst Du Kamillentee?",
+            "Frage 3: Magst Du Grünen Tee?",
+            "Frage 4: Magst Du Schwarzen Tee?",
+            "Frage 5: Magst Du Früchtetee?",
+            "Frage 6: Magst Du Fencheltee??",
+            "Frage 7: Magst Du Rooibusch-Tee? ",
+            "Frage 8: Magst Du Earl-Grey Tee?",
+            "Frage 9: Magst Du Pu Erh Tee?",
+            "Frage 10: Magst Du Vanille-Tee? ",
+            "Frage 11: Magst Du Ingwer-Tee?",
+            "Frage 12: Magst Du Yogi-Tee?",
+            "Frage 13: Magst Du Kokos-Mandel-Tee?",
+            "Frage 14: Magst Du Holunderblüten-Tee?",
+            "Frage 15: Magst Du Brennessel-Tee?",
+            "Frage 16: Magst Du Apfelsaft?",
+            "Frage 17: Magst Du Orangensaft? ",
+            "Frage 18: Magst Du Traubensaft?",
+            "Frage 19: Magst Du Kirschsaft?",
+            "Frage 20: Magst Du Bananensaft?",
+            "Frage 21: Magst Du Tomatensaft?",
+            "Frage 22: Magst Du Pfirsichsaft?",
+            "Frage 23: Magst Du Aprikosensaft?",
+            "Frage 24: Magst Du Mangosaft?",
+            "Frage 25: Magst Du Karottensaft?",
+            "Frage 26: Magst Du Gemüsesaft?",
+            "Frage 27: Magst Du Sauerkrautsaft? ",
+            "Frage 28: Magst Du Ananassaft?",
+            "Frage 29: Magst Du Papayasaft?",
+            "Frage 30: Magst Du Birnensaft?",
+            "Frage 31: Magst Du Vita Cola?",
+            "Frage 32: Magst Du Coca Cola?",
+            "Frage 33: Magst Du Pepsi Cola?",
+            "Frage 34: Magst Du Sprite?",
+            "Frage 35: Magst Du Fanta?",
+            "Frage 36: Magst Du Fanta Orange?",
+            "Frage 37: Magst Du Fanta Grapefruit? ",
+            "Frage 38: Magst Du Red Bull?",
+            "Frage 39: Magst Du Mezzo Mix?",
+            "Frage 40: Magst Du Isostar? ",
+            "Frage 41: Magst Du Bluna?",
+            "Frage 42: Magst Du Bionade?",
+            "Frage 43: Magst Du Bitter Lemon?",
+            "Frage 44: Magst Du Ginger Ale?",
+            "Frage 45: Magst Du Sinalco?");
+    //    JProgressBar proba; //Statusanzeige
 
 
 
 
 
-    final String scripturlstring = "http://10.42.0.41/zurmo/receive.php";
+    final String scripturlstring = "http://10.42.0.41/zurmo/test9.php";//receive.php";
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -73,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (internetAvailable()) {
-                    sendToServer(et.getText().toString(),null,null);
+                    sendToServer(et.getText().toString(),null,null,null);
                 } else {
                     Toast.makeText(getApplicationContext(), "Internet ist nicht verfügbar.", Toast.LENGTH_SHORT).show();
                 }
@@ -86,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
                 if (internetAvailable()) {
                     qcount++;
                     jcount++;
-                    sendToServer(et.getText().toString(),tvq.getText().toString(),btnj.getText().toString());
+                    sendToServer(et.getText().toString(),tvq.getText().toString(),btnj.getText().toString(),Integer.toString(qcount));
                     qvector++;
                     tvq.setText(questions.get(qvector));
                 } else {
@@ -101,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
                 if (internetAvailable()) {
                     qcount++;
                     ncount++;
-                    sendToServer(et.getText().toString(),tvq.getText().toString(),btnn.getText().toString());
+                    sendToServer(et.getText().toString(),tvq.getText().toString(),btnn.getText().toString(),Integer.toString(qcount));
 //                    sendToServer(tvq.getText().toString());
 //                    sendToServer(Integer.toString(ncount));
 //                    sendToServer(btnn.getText().toString());
@@ -119,13 +164,13 @@ public class MainActivity extends AppCompatActivity {
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void sendToServer(final String User, final String Frage, final String Antwort) {
+    public void sendToServer(final String User, final String Frage, final String Antwort, final String Fragenr) {
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    String textparam = "User=" + URLEncoder.encode(User, "UTF-8") +"&Frage=" + URLEncoder.encode(Frage, "UTF-8") + "&Antwort=" + URLEncoder.encode(Antwort, "UTF-8");
+                    String textparam = "User=" + URLEncoder.encode(User, "UTF-8") +"&Frage=" + URLEncoder.encode(Frage, "UTF-8") +"&Antwort=" + URLEncoder.encode(Antwort, "UTF-8") + "&Fragenr" + URLEncoder.encode(Fragenr, "UTF-8");
 
                     URL scripturl = new URL(scripturlstring);
                     HttpURLConnection connection = (HttpURLConnection) scripturl.openConnection();
